ff1a79d145f8e489bf137cab93e4657085cc0bb2
Boris <boris2000n@gmail.com>
2020-11-07 02:17:20 +0300

    Readme update. Main template done. NotImplemented yet

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 
 
+def read_input_data() -> (str, str, int):
+    """Read input parameters for task solution
+    alpha - regex in alphabet {a, b, c, 1, ., +, *}
+    x - char
+    k - natural number
+    returns (alpha, x, k)"""
+    raise NotImplementedError
+
+
+def solution():
+    alpha, x, k = read_input_data()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    solution()
