d5b36802d41187b280fa9536fd9f74df55c52dd1
Boris <boris2000n@gmail.com>
2020-11-09 20:29:52 +0300

    tests done

--- tests/tests.py
+++ tests/tests.py
@@ -13,13 +13,13 @@ def test_example():
 
 
 def test_other_examples():
+    assert string_solution('a a 1') == 1
     assert string_solution('abc.. a 1') == 3
     assert string_solution('abb.+ b 2') == 2
     assert string_solution('ab.b. b 2') == 3
+    assert string_solution('aaaa... a 3') == 4
+    assert string_solution('ab.bba..+* b 3') == 5
+    assert string_solution('aa.aaaa...+* a 5') == 6
     assert string_solution('ab.b. c 2') is None
     assert string_solution('b* a 1') is None
-    assert string_solution('ab.bba..+* b 3') == 5
     assert string_solution('a b 1') is None
-    assert string_solution('a a 1') == 1
-    assert string_solution('aaaa... a 3') == 4
-    assert string_solution('aa.aaaa...+* a 5') == 6
